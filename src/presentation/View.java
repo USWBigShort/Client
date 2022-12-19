79a48fa7f5e96bd9c039a1c49b506c0cb9b73d33
koownij <koownij@kakao.com>
2022-12-19 20:21:26 +0900

    refactor: View 코드 리팩토링

--- src/presentation/View.java
+++ src/presentation/View.java
@@ -4,79 +4,140 @@ import javax.swing.*;
 import java.awt.*;
 
 public class View extends JFrame {
-    private DefaultListModel model;
+
+    private DefaultListModel coinModel;
     private JList coinList;
     private JScrollPane coinListScrollView;
+
+    private JPanel coinInfoPanel;
+
+    private JPanel coinHoldingsPanel;
+    private JLabel coinHoldingsTitle;
+    private JLabel coinHoldingsValue;
+
+    private JPanel averagePurchasePricePanel;
+    private JLabel averagePurchasePriceTitle;
+    private JLabel averagePurchasePriceValue;
+
+    private JPanel coinProfitPanel;
+    private JLabel coinProfitTitle;
+    private JLabel coinProfitValue;
+
+    private JLabel dividingLine;
+
+    private JPanel availableAssetsPanel;
+    private JLabel availableAssetsTitle;
+    private JLabel availableAssetsValue;
+
+    private JPanel buyOrSellPanel;
+    private JLabel buyOrSellTitle;
+    private JTextField buyOrSellValue;
+
+    private JButton buyButton;
+    private JButton sellButton;
+
+    private JTextArea noticeTextArea;
+    private JScrollPane noticeScrollView;
+
+    private JPanel sendCommandPanel;
+    private JTextField sendCommandValue;
+    private JButton sendCommandButton;
+
+    private JPanel noticeTotalPanel;
 
     public View(String title) throws HeadlessException {
         super(title);
         setBounds(0, 0, 1000, 500);
         setLayout(new BorderLayout());
 
-        JPanel coinInfoPanel = new JPanel();
+        setCoinInfoPanel();
+
+        //TODO TEST UPDATE ALL COIN INFO
+        for(int i = 0; i <= 100; ++i) {
+            coinModel.addElement("test" + i);
+        }
+
+        setCoinHoldingsPanel();
+        setAveragePurchasePricePanel();
+        setCoinProfitPanel();
+        dividingLine = new JLabel("---");
+        coinInfoPanel.add(dividingLine);
+        setAvailableAssetsPanel();
+        setBuyOrSellPanel();
+        add(coinInfoPanel, BorderLayout.WEST);
+        setNoticePanel();
+
+        add(noticeTotalPanel, BorderLayout.EAST);
+
+        pack();
+    }
+
+    private void setCoinInfoPanel() {
+        coinInfoPanel = new JPanel();
         coinInfoPanel.setLayout(new BoxLayout(coinInfoPanel, BoxLayout.Y_AXIS));
 
-        model = new DefaultListModel();
-        coinList = new JList(model);
+        coinModel = new DefaultListModel();
+        coinList = new JList(coinModel);
         coinList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-
         coinListScrollView =new JScrollPane(coinList);
         coinListScrollView.setBorder(BorderFactory.createEmptyBorder(0,10,10,10));
 
         coinInfoPanel.add(coinListScrollView, BorderLayout.NORTH);
+    }
 
-        //TODO TEST UPDATE ALL COIN INFO
-        for(int i = 0; i <= 100; ++i) {
-            model.addElement("test" + i);
-        }
-
-        JPanel coinHoldingsPanel = new JPanel();
+    private void setCoinHoldingsPanel() {
+        coinHoldingsPanel = new JPanel();
         coinHoldingsPanel.setLayout(new FlowLayout());
-        JLabel coinHoldingsTitle = new JLabel("보유량 :");
-        JLabel coinHoldingsValue = new JLabel("0");
+        coinHoldingsTitle = new JLabel("보유량 :");
+        coinHoldingsValue = new JLabel("0");
 
         coinHoldingsPanel.add(coinHoldingsTitle);
         coinHoldingsPanel.add(coinHoldingsValue);
 
         coinInfoPanel.add(coinHoldingsPanel);
+    }
 
-        JPanel averagePurchasePricePanel = new JPanel();
-        coinHoldingsPanel.setLayout(new FlowLayout());
-        JLabel averagePurchasePriceTitle = new JLabel("평균매수가 :");
-        JLabel averagePurchasePriceValue = new JLabel("0");
+    private void setAveragePurchasePricePanel() {
+        averagePurchasePricePanel = new JPanel();
+        averagePurchasePricePanel.setLayout(new FlowLayout());
+        averagePurchasePriceTitle = new JLabel("평균매수가 :");
+        averagePurchasePriceValue = new JLabel("0");
         averagePurchasePricePanel.add(averagePurchasePriceTitle);
         averagePurchasePricePanel.add(averagePurchasePriceValue);
 
         coinInfoPanel.add(averagePurchasePricePanel);
+    }
 
-        JPanel coinProfitPanel = new JPanel();
+    private void setCoinProfitPanel() {
+        coinProfitPanel = new JPanel();
         coinHoldingsPanel.setLayout(new FlowLayout());
-        JLabel coinProfitTitle = new JLabel("수익 :");
-        JLabel coinProfitValue = new JLabel("0");
+        coinProfitTitle = new JLabel("수익 :");
+        coinProfitValue = new JLabel("0");
         coinProfitPanel.add(coinProfitTitle);
         coinProfitPanel.add(coinProfitValue);
 
         coinInfoPanel.add(coinProfitPanel);
+    }
 
-        JLabel dividingLine = new JLabel("---");
-        coinInfoPanel.add(dividingLine);
-
-        JPanel availableAssetsPanel = new JPanel();
+    private void setAvailableAssetsPanel() {
+        availableAssetsPanel = new JPanel();
         availableAssetsPanel.setLayout(new FlowLayout());
-        JLabel availableAssetsTitle = new JLabel("가용 자산 :");
-        JLabel availableAssetsValue = new JLabel("0");
+        availableAssetsTitle = new JLabel("가용 자산 :");
+        availableAssetsValue = new JLabel("0");
         availableAssetsPanel.add(availableAssetsTitle);
         availableAssetsPanel.add(availableAssetsValue);
 
         coinInfoPanel.add(availableAssetsPanel);
+    }
 
-        JPanel buyOrSellPanel = new JPanel();
+    private void setBuyOrSellPanel() {
+        buyOrSellPanel = new JPanel();
         buyOrSellPanel.setLayout(new FlowLayout());
-        JLabel buyOrSellTitle = new JLabel("매수/매도 수량 :");
-        JTextField buyOrSellValue = new JTextField(10);
+        buyOrSellTitle = new JLabel("매수/매도 수량 :");
+        buyOrSellValue = new JTextField(10);
 
-        JButton buyButton = new JButton("매수");
-        JButton sellButton = new JButton("매도");
+        buyButton = new JButton("매수");
+        sellButton = new JButton("매도");
 
         buyOrSellPanel.add(buyOrSellTitle);
         buyOrSellPanel.add(buyOrSellValue);
@@ -84,28 +145,24 @@ public class View extends JFrame {
         buyOrSellPanel.add(sellButton);
 
         coinInfoPanel.add(buyOrSellPanel);
+    }
 
-        add(coinInfoPanel, BorderLayout.WEST);
-
-        JTextArea noticeTextArea = new JTextArea();
-        JScrollPane noticeScrollView = new JScrollPane(noticeTextArea);
+    private void setNoticePanel() {
+        noticeTextArea = new JTextArea();
+        noticeScrollView = new JScrollPane(noticeTextArea);
         noticeScrollView.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         noticeTextArea.setEditable(false);
 
-        JPanel sendCommandPanel = new JPanel();
+        sendCommandPanel = new JPanel();
         sendCommandPanel.setLayout(new FlowLayout());
-        JTextField sendCommandValue = new JTextField(15);
-        JButton sendCommandButton = new JButton("전송");
+        sendCommandValue = new JTextField(15);
+        sendCommandButton = new JButton("전송");
         sendCommandPanel.add(sendCommandValue);
         sendCommandPanel.add(sendCommandButton);
 
-        JPanel noticeTotalPanel = new JPanel();
+        noticeTotalPanel = new JPanel();
         noticeTotalPanel.setLayout(new BoxLayout(noticeTotalPanel, BoxLayout.Y_AXIS));
         noticeTotalPanel.add(noticeScrollView);
         noticeTotalPanel.add(sendCommandPanel);
-
-        add(noticeTotalPanel, BorderLayout.EAST);
-
-        pack();
     }
 }
