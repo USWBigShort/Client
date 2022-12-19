0ee5ed33f351c86f9f18516edf61f7f29bdb8bf3
koownij <koownij@kakao.com>
2022-12-19 20:21:54 +0900

    feat: UserInfoSocket 추가

---
+++ src/data/UserInfoSocket.java
@@ -0,0 +1,28 @@
+package data;
+
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.Socket;
+
+public class UserInfoSocket {
+    int port = 8888;
+    String ip = "127.0.0.1";
+    Socket socket;
+
+    private static UserInfoSocket instance = new UserInfoSocket();
+
+    UserInfoSocket() {
+        try {
+            InetAddress ipAddress = InetAddress.getByName(ip);
+            socket = new Socket(ipAddress,port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static UserInfoSocket getInstance() {
+        return instance;
+    }
+}
